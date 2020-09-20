a608669bc53a7e1c8951d394fc9c756b384e588f
JackRKelly <kcjackkelly@gmail.com>
2020-09-20 13:18:32 -0500

    Added navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Logo</li>
+        <li>Github</li>
+        <li>Dev.to</li>
+        <li>LinkedIn</li>
+      </ul>
+      <ul>
+        <li>About</li>
+        <li>Work</li>
+        <li>Articles</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
