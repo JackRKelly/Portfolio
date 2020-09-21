4fa3dd084741f9ff545280594cd5b0f5daa4bea8
JackRKelly <kcjackkelly@gmail.com>
2020-09-20 19:04:56 -0500

    Added mobile navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "./components/Navigation";
 import "./App.scss";
 import Home from "./Home";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const checkMobile = () => {
+    if (window.innerWidth < 600) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  window.addEventListener("resize", checkMobile);
+
+  useEffect(() => {
+    checkMobile();
+  });
+
   return (
     <div className="App">
-      <Navigation />
+      <Navigation isMobile={isMobile} />
       <Home />
     </div>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,79 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
 import DevTo from "../assets/svg/devto";
 import Github from "../assets/svg/github";
 import JRKLogo from "../assets/svg/jrklogo";
 import LinkedIn from "../assets/svg/linkedin";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
-const Navigation = () => {
+interface Props {
+  isMobile: boolean;
+}
+
+const Navigation = (props: Props) => {
+  const { isMobile } = props;
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
-    <nav>
-      <ul className="container">
-        <li className="left">
-          <ul>
-            <li className="logo">
-              <AnchorLink offset="100" href="#home">
-                <JRKLogo />
-              </AnchorLink>
-            </li>
-            <li className="divider">
-              <div></div>
-            </li>
-            <li className="icon">
-              <a
-                href="https://github.com/JackRKelly"
-                target="_blank"
-                rel="noopener noreferrer"
+    <>
+      {isMobile ? (
+        <>
+          <nav className="mobile">
+            <ul className="container">
+              <li className="left">
+                <ul>
+                  <li className="logo">
+                    <AnchorLink offset="100" href="#home">
+                      <JRKLogo />
+                    </AnchorLink>
+                  </li>
+                </ul>
+              </li>
+              <li className="right">
+                <ul>
+                  <li
+                    onClick={() => {
+                      setIsNavOpen(true);
+                    }}
+                  >
+                    Ham
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <nav
+            className="mobile-full"
+            style={{
+              width: isNavOpen ? "100%" : "0%",
+              pointerEvents: isNavOpen ? "auto" : "none",
+            }}
+          >
+            <ul>
+              <li
+                onClick={() => {
+                  setIsNavOpen(false);
+                }}
               >
-                <Github />
-              </a>
-            </li>
-            <li className="icon">
-              <a
-                href="https://dev.to/jackrkelly"
-                target="_blank"
-                rel="noopener noreferrer"
+                close
+              </li>
+              <li
+                onClick={() => {
+                  setIsNavOpen(false);
+                }}
               >
-                <DevTo />
-              </a>
-            </li>
-            <li className="icon">
-              <a
-                href="https://www.linkedin.com/in/jackkelly4/"
-                target="_blank"
-                rel="noopener noreferrer"
+                <AnchorLink offset="70" href="#about">
+                  About
+                </AnchorLink>
+              </li>
+              <li
+                onClick={() => {
+                  setIsNavOpen(false);
+                }}
               >
-                <LinkedIn />
-              </a>
+                <AnchorLink offset="70" href="#work">
+                  Work
+                </AnchorLink>
+              </li>
+              <li
+                onClick={() => {
+                  setIsNavOpen(false);
+                }}
+              >
+                <AnchorLink offset="70" href="#articles">
+                  Articles
+                </AnchorLink>
+              </li>
+              <li
+                onClick={() => {
+                  setIsNavOpen(false);
+                }}
+              >
+                <AnchorLink offset="70" href="#contact">
+                  Contact
+                </AnchorLink>
+              </li>
+            </ul>
+          </nav>
+        </>
+      ) : (
+        <nav className="desktop">
+          <ul className="container">
+            <li className="left">
+              <ul>
+                <li className="logo">
+                  <AnchorLink offset="100" href="#home">
+                    <JRKLogo />
+                  </AnchorLink>
+                </li>
+                <li className="divider">
+                  <div></div>
+                </li>
+                <li className="icon">
+                  <a
+                    href="https://github.com/JackRKelly"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Github />
+                  </a>
+                </li>
+                <li className="icon">
+                  <a
+                    href="https://dev.to/jackrkelly"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <DevTo />
+                  </a>
+                </li>
+                <li className="icon">
+                  <a
+                    href="https://www.linkedin.com/in/jackkelly4/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <LinkedIn />
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="right">
+              <ul>
+                <li>
+                  <AnchorLink offset="70" href="#about">
+                    About
+                  </AnchorLink>
+                </li>
+                <li>
+                  <AnchorLink offset="70" href="#work">
+                    Work
+                  </AnchorLink>
+                </li>
+                <li>
+                  <AnchorLink offset="70" href="#articles">
+                    Articles
+                  </AnchorLink>
+                </li>
+                <li>
+                  <AnchorLink offset="70" href="#contact">
+                    Contact
+                  </AnchorLink>
+                </li>
+              </ul>
             </li>
           </ul>
-        </li>
-        <li className="right">
-          <ul>
-            <li>
-              <AnchorLink offset="70" href="#about">
-                About
-              </AnchorLink>
-            </li>
-            <li>
-              <AnchorLink offset="70" href="#work">
-                Work
-              </AnchorLink>
-            </li>
-            <li>
-              <AnchorLink offset="70" href="#articles">
-                Articles
-              </AnchorLink>
-            </li>
-            <li>
-              <AnchorLink offset="70" href="#contact">
-                Contact
-              </AnchorLink>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+        </nav>
+      )}
+    </>
   );
 };
 
