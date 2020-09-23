70f557aebb9f67b8c8a1a678554330ea2b4ccf43
JackRKelly <kcjackkelly@gmail.com>
2020-09-23 10:35:29 -0500

    Added svg icons with correct casing

---
+++ src/assets/svg/DownArrow.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+
+const DownArrow: FC = () => {
+  return (
+    <svg
+      id="Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 54 54"
+    >
+      <defs></defs>
+      <polygon
+        className="arrow-1"
+        points="27 41.17 27 41.17 27 41.17 40.68 27.5 37.14 23.96 29.5 31.6 29.5 12.83 24.5 12.83 24.5 31.6 16.86 23.96 13.32 27.5 27 41.17"
+      />
+      <polygon
+        className="arrow-2"
+        points="27 41.17 27 41.17 27 41.17 40.68 27.5 37.14 23.96 29.5 31.6 29.5 12.83 24.5 12.83 24.5 31.6 16.86 23.96 13.32 27.5 27 41.17"
+      />
+      <g id="Ellipse_9" data-name="Ellipse 9">
+        <circle style={{ fill: "none" }} cx="27" cy="27" r="27" />
+        <path
+          d="M27.65,54.87a27,27,0,1,1,27-27A27,27,0,0,1,27.65,54.87Zm0-50a23,23,0,1,0,23,23A23,23,0,0,0,27.65,4.87Z"
+          transform="translate(-0.65 -0.87)"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default DownArrow;
