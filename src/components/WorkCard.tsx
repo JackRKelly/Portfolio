d1cf951c100476c81489882d4538d07daa60028a
JackRKelly <kcjackkelly@gmail.com>
2020-10-03 18:51:09 -0500

    Added work card modal structure

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 import PlaceholderImage from "../assets/images/placeholder.png";
 import ExpandWork from "../assets/svg/ExpandWork";
@@ -12,26 +12,39 @@ interface Props {
 const WorkCard: FC<Props> = (props: Props) => {
   const { description, tags, title } = props;
 
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
-    <li>
-      <div className="image">
-        <img src={PlaceholderImage} alt="" />
-        <ul className="tag-list">
-          {tags.map((tag, index) => (
-            <li key={index}>{tag}</li>
-          ))}
-        </ul>
-      </div>
-      <div className="text">
-        <h5>{title}</h5>
-        <p>{description}</p>
-        <div className="link">
-          <p>
-            More Details <ExpandWork />
-          </p>
+    <>
+      <div className="work-details">
+        <div className="content">
+          <p>details</p>
         </div>
       </div>
-    </li>
+      <li
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        <div className="image">
+          <img src={PlaceholderImage} alt="" />
+          <ul className="tag-list">
+            {tags.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="text">
+          <h5>{title}</h5>
+          <p>{description}</p>
+          <div className="link">
+            <p>
+              More Details <ExpandWork />
+            </p>
+          </div>
+        </div>
+      </li>
+    </>
   );
 };
 
