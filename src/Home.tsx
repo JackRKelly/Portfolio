4854e676834045a421cf4792869696f8e0597638
JackRKelly <kcjackkelly@gmail.com>
2020-10-08 13:57:26 -0500

    Added social links to contact form

--- src/Home.tsx
+++ src/Home.tsx
@@ -178,6 +178,78 @@ const Home: FC<Props> = (props: Props) => {
                 e.preventDefault();
               }}
             >
+              <ul className="contact-options">
+                <li>
+                  <a
+                    href="mailto:kcjackkelly@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="submit-button"
+                  >
+                    <div className="icon">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="#fff"
+                          d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"
+                        />
+                      </svg>
+                    </div>
+                    <p>kcjackkelly@gmail.com</p>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/JackRKelly"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="submit-button"
+                  >
+                    <div className="icon">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="#fff"
+                          d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                        />
+                      </svg>
+                    </div>
+                    <p>JackRKelly</p>
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/jackkelly4/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="submit-button"
+                  >
+                    <div className="icon">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="#fff"
+                          d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"
+                        />
+                      </svg>
+                    </div>
+                    <p>Jack Kelly</p>
+                  </a>
+                </li>
+              </ul>
               <input type="hidden" name="form-name" value="contact" />
               <input
                 type="text"
@@ -210,26 +282,23 @@ const Home: FC<Props> = (props: Props) => {
                 }}
                 required
               ></textarea>
-
               <button type="submit" className="submit-button">
-                <div className="submit">
-                  <div className="icon">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="44"
-                      height="44"
-                      viewBox="0 0 44 44"
-                    >
-                      <path
-                        id="plane"
-                        d="M44,0,33,40.333,18.1,27.062l14.3-15.1L13.228,25.216,0,22ZM16.5,30.558V44l5.973-8.123Z"
-                        fill="#fff"
-                      />
-                    </svg>
-                  </div>
-
-                  <p>Send Message</p>
+                <div className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="44"
+                    height="44"
+                    viewBox="0 0 44 44"
+                  >
+                    <path
+                      id="plane"
+                      d="M44,0,33,40.333,18.1,27.062l14.3-15.1L13.228,25.216,0,22ZM16.5,30.558V44l5.973-8.123Z"
+                      fill="#fff"
+                    />
+                  </svg>
                 </div>
+
+                <p>Send Message</p>
               </button>
             </form>
           </div>
