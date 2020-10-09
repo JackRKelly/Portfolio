62f995bbd5a5cba0a19b30e190a40a8402a69e26
JackRKelly <kcjackkelly@gmail.com>
2020-10-08 19:12:30 -0500

    Filled SVG outline assets

--- src/assets/svg/Hamburger.tsx
+++ src/assets/svg/Hamburger.tsx
@@ -8,19 +8,9 @@ const Hamburger = () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1043.68 999"
     >
-      <defs></defs>
-      <path
-        d="M998.68,58v91.5H55V58H998.68m50-50H5V199.5H1048.68V8Z"
-        transform="translate(-5 -8)"
-      />
-      <path
-        d="M998.68,460.75v91.5H55v-91.5H998.68m50-50H5v191.5H1048.68V410.75Z"
-        transform="translate(-5 -8)"
-      />
-      <path
-        d="M998.68,865.5V957H55V865.5H998.68m50-50H5V1007H1048.68V815.5Z"
-        transform="translate(-5 -8)"
-      />
+	<polygon points="1043.7,0 0,0 0,191.5 1043.7,191.5 1043.7,0 	"/>
+	<polygon points="0,402.8 0,594.2 1043.7,594.2 1043.7,402.8 1043.7,402.8 	"/>
+	<polygon points="0,807.5 0,999 1043.7,999 1043.7,807.5 1043.7,807.5 	"/>
     </svg>
   );
 };
