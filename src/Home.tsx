758707e83e170c40a398fab06a20c0d1799b098a
JackRKelly <kcjackkelly@gmail.com>
2020-10-11 17:03:07 -0500

    Added resume download to contact form

--- src/Home.tsx
+++ src/Home.tsx
@@ -361,30 +361,6 @@ const Home: FC<Props> = (props: Props) => {
                   </li>
                   <li>
                     <a
-                      href="https://github.com/JackRKelly"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="submit-button"
-                    >
-                      <div className="icon">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            fill="#fff"
-                            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-                          />
-                        </svg>
-                      </div>
-                      <p>JackRKelly</p>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
                       href="https://www.linkedin.com/in/jackkelly4/"
                       target="_blank"
                       rel="noopener noreferrer"
@@ -404,6 +380,28 @@ const Home: FC<Props> = (props: Props) => {
                         </svg>
                       </div>
                       <p>Jack Kelly</p>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/pdf/jack-kelly-resume.pdf"
+                      download
+                      className="submit-button"
+                    >
+                      <div className="icon">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fill="#fff"
+                            d="M15.003 3h2.997v5h-2.997v-5zm8.997 1v20h-24v-24h20l4 4zm-19 5h14v-7h-14v7zm16 4h-18v9h18v-9z"
+                          />
+                        </svg>
+                      </div>
+                      <p>Résumé</p>
                     </a>
                   </li>
                 </ul>
@@ -463,8 +461,10 @@ const Home: FC<Props> = (props: Props) => {
         </section>
         <footer id="footer">
           <Content>
-            <div className="jrk-logo">
-              <p>Download resume</p>
+            <div className="resume-download">
+              <a href="/pdf/jack-kelly-resume.pdf" download>
+                Download Resume
+              </a>
             </div>
           </Content>
         </footer>
