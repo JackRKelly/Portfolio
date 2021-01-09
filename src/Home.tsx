20805b82f7dfa847be0464c05626360d0614558e
JackRKelly <kcjackkelly@gmail.com>
2021-01-08 21:12:50 -0600

    Updated about me copy text

--- src/Home.tsx
+++ src/Home.tsx
@@ -238,7 +238,7 @@ const Home: FC<Props> = (props: Props) => {
               I'm a self-taught software engineer who is currently working at
               Cerner. In my free time, I enjoy contributing to open-source,
               designing, and working on side projects. Some technologies I enjoy
-              are Typescript, Rust, Docker, React.js, and Node.js. I have been
+              are Typescript, Rust, React.js, Ruby, and Node.js. I have been
               writing code for 5 years since I first got introduced to web
               development.
             </p>
