cb17ef56d66b593b81e2a1beb0c1b37041360837
JackRKelly <kcjackkelly@gmail.com>
2021-07-12 13:52:09 -0500

    Update technologies about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -17,7 +17,7 @@ export const About: FC<Props> = ({ isMobile, aboutRef }) => {
           I'm a self-taught software engineer who is currently looking for work.
           In my free time, I enjoy contributing to open-source, designing, and
           working on side projects. Some technologies I enjoy are Typescript,
-          Rust, C#, and Ruby. I have been writing code for 5 years since I first
+          Golang, and Rust. I have been writing code for 5 years since I first
           got introduced to web development.
         </p>
       </Content>
